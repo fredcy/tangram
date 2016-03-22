203e2d229eecc6bc9854db7a7212b4bb72efe077
Fred Yankowski <fcy@imsa.edu>
2016-03-22 12:36:56 -0500

    Fake the separation between pieces using a white path around the boarder of each.

--- Tangram.elm
+++ Tangram.elm
@@ -2,7 +2,7 @@ module Main (..) where
 
 import Window
 import Color exposing (Color, rgb)
-import Graphics.Collage exposing (Form, collage, filled, polygon, scale)
+import Graphics.Collage exposing (Form, collage, defaultLine, filled, group, move, outlined, path, polygon, scale, solid, traced)
 import Graphics.Element exposing (Element)
 
 
@@ -11,22 +11,32 @@ import Graphics.Element exposing (Element)
 
 elmGreen : Color
 elmGreen =
-    rgb 141 215 55
+  rgb 141 215 55
 
 
 elmTurquoise : Color
 elmTurquoise =
-    rgb 96 181 204
+  rgb 96 181 204
 
 
 elmOrange : Color
 elmOrange =
-    rgb 239 165 0
+  rgb 239 165 0
 
 
 elmGray : Color
 elmGray =
-    rgb 90 99 120
+  rgb 90 99 120
+
+
+red : Color
+red =
+  rgb 255 0 0
+
+
+white : Color
+white =
+  rgb 255 255 255
 
 
 
@@ -35,45 +45,101 @@ elmGray =
 
 scalingFactor : ( Int, Int ) -> Float
 scalingFactor ( width, height ) =
-    (min (toFloat width) (toFloat height)) / 3
+  (min (toFloat width) (toFloat height)) / 3
 
 
 filledForm : Float -> Color -> List ( Float, Float ) -> Form
 filledForm scaleFactor color points =
-    scale scaleFactor (filled color (polygon points))
+  let
+    linestyle =
+      { defaultLine | width = separation, color = white, join = Graphics.Collage.Smooth }
+
+    connect points =
+      case List.head points of
+        Nothing ->
+          []
+
+        Just h ->
+          List.append points [ h ]
+  in
+    group
+      [ polygon points |> filled color
+      , path (connect points) |> traced linestyle
+      ]
 
 
-logo : Float -> List Form
+separation =
+  5.0e-2
+
+
+logo : Float -> Form
 logo scaleFactor =
-    [ (filledForm scaleFactor elmTurquoise [ ( 0, 0 ), ( -1, -1 ), ( 1, -1 ) ])
-    , (filledForm scaleFactor elmGray [ ( 0, 0 ), ( -1, -1 ), ( -1, 1 ) ])
-    , (filledForm scaleFactor elmTurquoise [ ( 1, 1 ), ( 0, 1 ), ( 1, 0 ) ])
-    , (filledForm scaleFactor elmOrange [ ( 1, -1 ), ( 1, 0 ), ( 0.5, -0.5 ) ])
-    , (filledForm scaleFactor elmOrange [ ( 0, 0 ), ( 0.5, 0.5 ), ( -0.5, 0.5 ) ])
-    , (filledForm scaleFactor elmGreen [ ( 0, 0 ), ( 0.5, 0.5 ), ( 1, 0 ), ( 0.5, -0.5 ) ])
-    , (filledForm scaleFactor elmGreen [ ( 0, 1 ), ( -1, 1 ), ( -0.5, 0.5 ), ( 0.5, 0.5 ) ])
+  group
+    [ bigTriangleTurquoise scaleFactor
+    , bigTriangleGray scaleFactor
+    , mediumTriangle scaleFactor
+    , smallTriangleSE scaleFactor
+    , smallTriangleN scaleFactor
+    , square scaleFactor
+    , parallelogram scaleFactor
     ]
+    |> scale scaleFactor
+
+
+bigTriangleTurquoise : Float -> Form
+bigTriangleTurquoise scaleFactor =
+  filledForm scaleFactor elmTurquoise [ ( 0, 0 ), ( -1, -1 ), ( 1, -1 ) ]
+
+
+bigTriangleGray : Float -> Form
+bigTriangleGray scaleFactor =
+  filledForm scaleFactor elmGray [ ( 0, 0 ), ( -1, -1 ), ( -1, 1 ) ]
+
+
+mediumTriangle : Float -> Form
+mediumTriangle scaleFactor =
+  filledForm scaleFactor elmTurquoise [ ( 1, 1 ), ( 0, 1 ), ( 1, 0 ) ]
+
+
+smallTriangleSE : Float -> Form
+smallTriangleSE scaleFactor =
+  filledForm scaleFactor elmOrange [ ( 1, -1 ), ( 1, 0 ), ( 0.5, -0.5 ) ]
+
+
+smallTriangleN : Float -> Form
+smallTriangleN scaleFactor =
+  filledForm scaleFactor elmOrange [ ( 0, 0 ), ( 0.5, 0.5 ), ( -0.5, 0.5 ) ]
+
+
+square : Float -> Form
+square scaleFactor =
+  filledForm scaleFactor elmGreen [ ( 0, 0 ), ( 0.5, 0.5 ), ( 1, 0 ), ( 0.5, -0.5 ) ]
+
+
+parallelogram : Float -> Form
+parallelogram scaleFactor =
+  filledForm scaleFactor elmGreen [ ( 0, 1 ), ( -1, 1 ), ( -0.5, 0.5 ), ( 0.5, 0.5 ) ]
 
 
 
 --functions
 
 
-displayCentered : ( Int, Int ) -> List Form -> Element
-displayCentered ( width, height ) forms =
-    collage width height forms
+displayCentered : ( Int, Int ) -> Form -> Element
+displayCentered ( width, height ) form =
+  collage width height [ form ]
 
 
 updatedScalingFactor : Signal Float
 updatedScalingFactor =
-    (Signal.map scalingFactor Window.dimensions)
+  (Signal.map scalingFactor Window.dimensions)
 
 
-scaledLogo : Signal (List Form)
+scaledLogo : Signal Form
 scaledLogo =
-    (Signal.map logo updatedScalingFactor)
+  (Signal.map logo updatedScalingFactor)
 
 
 main : Signal Element
 main =
-    Signal.map2 displayCentered Window.dimensions scaledLogo
+  Signal.map2 displayCentered Window.dimensions scaledLogo
