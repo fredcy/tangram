41a3620b0cb8df80dce835a25bda67d1712cebcc
Fred Yankowski <fcy@imsa.edu>
2016-03-22 12:54:29 -0500

    Remove extraneous scalingFactor parameters.

---
+++ Colors.elm
@@ -0,0 +1,33 @@
+module Colors (..) where
+
+import Color exposing (Color, rgb)
+
+
+elmGreen : Color
+elmGreen =
+  rgb 141 215 55
+
+
+elmTurquoise : Color
+elmTurquoise =
+  rgb 96 181 204
+
+
+elmOrange : Color
+elmOrange =
+  rgb 239 165 0
+
+
+elmGray : Color
+elmGray =
+  rgb 90 99 120
+
+
+red : Color
+red =
+  rgb 255 0 0
+
+
+white : Color
+white =
+  rgb 255 255 255
--- Tangram.elm
+++ Tangram.elm
@@ -4,40 +4,7 @@ import Window
 import Color exposing (Color, rgb)
 import Graphics.Collage exposing (Form, collage, defaultLine, filled, group, move, outlined, path, polygon, scale, solid, traced)
 import Graphics.Element exposing (Element)
-
-
---colors
-
-
-elmGreen : Color
-elmGreen =
-  rgb 141 215 55
-
-
-elmTurquoise : Color
-elmTurquoise =
-  rgb 96 181 204
-
-
-elmOrange : Color
-elmOrange =
-  rgb 239 165 0
-
-
-elmGray : Color
-elmGray =
-  rgb 90 99 120
-
-
-red : Color
-red =
-  rgb 255 0 0
-
-
-white : Color
-white =
-  rgb 255 255 255
-
+import Colors exposing (..)
 
 
 --shapes
@@ -48,13 +15,14 @@ scalingFactor ( width, height ) =
   (min (toFloat width) (toFloat height)) / 3
 
 
-filledForm : Float -> Color -> List ( Float, Float ) -> Form
-filledForm scaleFactor color points =
+filledForm : Color -> List ( Float, Float ) -> Form
+filledForm color points =
   let
     linestyle =
       { defaultLine | width = separation, color = white, join = Graphics.Collage.Smooth }
 
-    connect points =
+    -- Add first point to end of list so path forms a loop.
+    loopedPoints =
       case List.head points of
         Nothing ->
           []
@@ -64,10 +32,11 @@ filledForm scaleFactor color points =
   in
     group
       [ polygon points |> filled color
-      , path (connect points) |> traced linestyle
+      , path loopedPoints |> traced linestyle
       ]
 
 
+separation : Float
 separation =
   5.0e-2
 
@@ -75,50 +44,50 @@ separation =
 logo : Float -> Form
 logo scaleFactor =
   group
-    [ bigTriangleTurquoise scaleFactor
-    , bigTriangleGray scaleFactor
-    , mediumTriangle scaleFactor
-    , smallTriangleSE scaleFactor
-    , smallTriangleN scaleFactor
-    , square scaleFactor
-    , parallelogram scaleFactor
+    [ bigTriangleTurquoise
+    , bigTriangleGray
+    , mediumTriangle
+    , smallTriangleSE
+    , smallTriangleN
+    , square
+    , parallelogram
     ]
     |> scale scaleFactor
 
 
-bigTriangleTurquoise : Float -> Form
-bigTriangleTurquoise scaleFactor =
-  filledForm scaleFactor elmTurquoise [ ( 0, 0 ), ( -1, -1 ), ( 1, -1 ) ]
+bigTriangleTurquoise : Form
+bigTriangleTurquoise =
+  filledForm elmTurquoise [ ( 0, 0 ), ( -1, -1 ), ( 1, -1 ) ]
 
 
-bigTriangleGray : Float -> Form
-bigTriangleGray scaleFactor =
-  filledForm scaleFactor elmGray [ ( 0, 0 ), ( -1, -1 ), ( -1, 1 ) ]
+bigTriangleGray : Form
+bigTriangleGray =
+  filledForm elmGray [ ( 0, 0 ), ( -1, -1 ), ( -1, 1 ) ]
 
 
-mediumTriangle : Float -> Form
-mediumTriangle scaleFactor =
-  filledForm scaleFactor elmTurquoise [ ( 1, 1 ), ( 0, 1 ), ( 1, 0 ) ]
+mediumTriangle : Form
+mediumTriangle =
+  filledForm elmTurquoise [ ( 1, 1 ), ( 0, 1 ), ( 1, 0 ) ]
 
 
-smallTriangleSE : Float -> Form
-smallTriangleSE scaleFactor =
-  filledForm scaleFactor elmOrange [ ( 1, -1 ), ( 1, 0 ), ( 0.5, -0.5 ) ]
+smallTriangleSE : Form
+smallTriangleSE =
+  filledForm elmOrange [ ( 1, -1 ), ( 1, 0 ), ( 0.5, -0.5 ) ]
 
 
-smallTriangleN : Float -> Form
-smallTriangleN scaleFactor =
-  filledForm scaleFactor elmOrange [ ( 0, 0 ), ( 0.5, 0.5 ), ( -0.5, 0.5 ) ]
+smallTriangleN : Form
+smallTriangleN =
+  filledForm elmOrange [ ( 0, 0 ), ( 0.5, 0.5 ), ( -0.5, 0.5 ) ]
 
 
-square : Float -> Form
-square scaleFactor =
-  filledForm scaleFactor elmGreen [ ( 0, 0 ), ( 0.5, 0.5 ), ( 1, 0 ), ( 0.5, -0.5 ) ]
+square : Form
+square =
+  filledForm elmGreen [ ( 0, 0 ), ( 0.5, 0.5 ), ( 1, 0 ), ( 0.5, -0.5 ) ]
 
 
-parallelogram : Float -> Form
-parallelogram scaleFactor =
-  filledForm scaleFactor elmGreen [ ( 0, 1 ), ( -1, 1 ), ( -0.5, 0.5 ), ( 0.5, 0.5 ) ]
+parallelogram : Form
+parallelogram =
+  filledForm elmGreen [ ( 0, 1 ), ( -1, 1 ), ( -0.5, 0.5 ), ( 0.5, 0.5 ) ]
 
 
 
